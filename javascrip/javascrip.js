2ab3eee3397524a7208f549af1590020ed62ef34
ricardovillasantivasques-hue <ricardovillasantivasques@gmail.com>
2025-12-06 15:57:41 -0300

    Add files via upload

---
+++ javascrip/javascrip.js
@@ -0,0 +1,6 @@
+function activarModoInclusivo() {
+    document.body.classList.toggle("modo-inclusivo");
+}
+function mostrarInfoCreador() {
+    alert("Creador del sitio:\nNombre: Ricardo Villasanti\nEdad: 17 años\nEmail: ricardovillasantivasques@gmail.com");
+}
